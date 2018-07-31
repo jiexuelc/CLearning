6d69a175144de7e8189400d424c8c65d28dda981
jiexuelc <jiexuelc@outlook.com>
2018-07-31 20:23:52 +0800

    gets_s.c

---
+++ strtok_r/gets_s.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "gets_s.h"
+
+char *gets_s(char *str, size_t num)
+{
+    if (0 != fgets(str, num, stdin))
+    {
+        size_t len = strlen(str);
+        if (len > 0 && str[len-1] == '\n')
+            str[len-1] = '\0';
+        return str;
+    }
+    return 0;
+}
