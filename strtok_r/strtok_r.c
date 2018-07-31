9af2659d2395523912968f832e6221fc95615f7f
jiexuelc <jiexuelc@outlook.com>
2018-07-31 20:23:12 +0800

    strtok_r.c

---
+++ strtok_r/strtok_r.c
@@ -0,0 +1,52 @@
+#include <string.h>
+#include "strtok_r.h"
+
+char *strtok_r(char *s, const char *delim, char **lasts)
+{
+	const char *spanp;
+	int c, sc;
+	char *tok;
+
+	/* s may be NULL */
+	/*assert(delim != NULL);*/
+	/*assert(lasts != NULL);*/
+
+	if (s == NULL && (s = *lasts) == NULL)
+		return (NULL);
+
+	/*
+	 * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
+	 */
+cont:
+	c = *s++;
+	for (spanp = delim; (sc = *spanp++) != 0;) {
+		if (c == sc)
+			goto cont;
+	}
+
+	if (c == 0) {		/* no non-delimiter characters */
+		*lasts = NULL;
+		return (NULL);
+	}
+	tok = s - 1;
+
+	/*
+	 * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
+	 * Note that delim must have one NUL; we stop if we see that, too.
+	 */
+	for (;;) {
+		c = *s++;
+		spanp = delim;
+		do {
+			if ((sc = *spanp++) == c) {
+				if (c == 0)
+					s = NULL;
+				else
+					s[-1] = 0;
+				*lasts = s;
+				return (tok);
+			}
+		} while (sc != 0);
+	}
+	/* NOTREACHED */
+}
