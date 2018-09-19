ca88f2501e82c8887afd3102e340b20444897c56
lichao <jiexuelc@outlook.com>
2018-09-19 16:34:47 +0800

    计算天数头文件

---
+++ myProject/Calculate_Days/days.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: days.h
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: 2018年04月09日 星期一 21时07分39秒
+	> Description: 计算天数头文件
+ ************************************************************************/
+
+#ifndef _DAYS_H
+#define _DAYS_H
+unsigned int days(unsigned int year,unsigned int month,unsigned int day);
+#endif
