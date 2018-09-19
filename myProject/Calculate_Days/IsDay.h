25d0fd20e7738b1832289169fb2c727b6cb7d1bc
lichao <jiexuelc@outlook.com>
2018-09-19 16:34:13 +0800

    判断日期是否合法头文件

---
+++ myProject/Calculate_Days/IsDay.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: IsDay.h
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: 2018年04月09日 星期一 20时14分48秒
+	> Description: 判断日期是否合法头文件
+ ************************************************************************/
+
+#ifndef _ISDAY_H
+#define _ISDAY_H
+typedef enum bool{TRUE=1,FALSE=0}bool;
+bool isdate(unsigned int year,unsigned int month,unsigned int day);
+#endif
