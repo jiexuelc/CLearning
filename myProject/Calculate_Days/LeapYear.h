87fbd2ab273f089f487737d2f49234bd322faf06
lichao <jiexuelc@outlook.com>
2018-09-19 16:33:54 +0800

    判断是否闰年头文件

---
+++ myProject/Calculate_Days/LeapYear.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: LeapYear.h
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: 2018年04月09日 星期一 20时15分05秒
+	> Description: 判断是否闰年头文件
+ ************************************************************************/
+
+#ifndef _LEAPYEAR_H
+#define _LEAPYEAR_H
+#include"IsDay.h"
+bool LeapYear(unsigned int year);
+#endif
