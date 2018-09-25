b4cac56caed7c195edd94fff78bbf2dcc64dcd84
lichao <jiexuelc@outlook.com>
2018-09-25 20:13:18 +0800

    sscanf()与sprintf()分别从字符串读取数据和将输出数据放到字符串

--- FileIO/sscanf_ssptintf.c
+++ FileIO/sscanf_ssptintf.c
@@ -3,7 +3,9 @@
 	> Author: jiexue
 	> Mail: jiexuelc@163.com
 	> Created Time: Tue 25 Sep 2018 02:40:24 PM CST
-	> Description: sscanf()与sprintf()分别从字符串读取数据和将输出数据放到字符串 
+	> Description: sscanf()与sprintf()分别从字符串读取数据和将输出数据放到字符串
+                        int sprintf(char *str, const char *format, ...);
+                        int sscanf(const char *str, const char *format, ...);
  ************************************************************************/
 
 #include <stdio.h>
