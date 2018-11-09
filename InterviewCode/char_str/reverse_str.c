fbb8e72cc6f0fe1d4c8ec42d550a8fdb6d44f2bf
lichao <jiexuelc@outlook.com>
2018-11-09 16:24:55 +0800

    将字符串反转输出

---
+++ InterviewCode/char_str/reverse_str.c
@@ -0,0 +1,40 @@
+/*************************************************************************
+    > File Name: reverse_str.c
+    > Author: jiexue
+    > Mail: jiexuelc@163.com
+    > Created Time: Wed 26 Sep 2018 09:19:35 PM CST
+    > Description: 将字符串反转进行输出
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * 将字符串反转
+ */
+void reverse_str(char* p)
+{
+    
+    int len = strlen(p);
+    int half = len / 2;
+    int i;
+    char temp;
+    
+    for(i = 0; i < half; i++){
+        temp = *(p+i);
+        *(p+i) = *(p+len-1-i);
+        *(p+len-1-i) = temp;
+    }
+}
+
+int main()
+{
+    char a[] = "hello world";
+    char b[] = "helloworld";
+    reverse_str(a);
+    reverse_str(b);
+    printf("%s\n", a);
+    printf("%s\n", b);
+    
+    return 0;
+}
