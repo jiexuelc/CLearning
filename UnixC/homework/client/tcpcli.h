757af62363d76a1c5309d135853e3e960374410f
jiexuelc <jiexuelc@outlook.com>
2019-05-18 21:38:48 +0800

    Create tcpcli.h

---
+++ UnixC/homework/client/tcpcli.h
@@ -0,0 +1,20 @@
+/** @file tcpcli.h
+ *  @note 
+ *  @brief 说明本文件
+ *  
+ *  @author jiexue
+ *  @date 2019年05月16日 星期四 21时01分38秒
+ *  
+ *  @note 
+ *  
+ *  @warning 
+ */
+
+#ifndef _TCPCLI_H
+#define _TCPCLI_H 1
+
+#include "common.h"
+
+void TCPService(stServerNode *pstServer);
+
+#endif
